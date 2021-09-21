7ae603070f7d4f43e102c0046fcc193c9460c080
Vladimir Iglovikov <Iglovikov@gmail.com>
2021-09-21 16:07:59 -0700

    Figured out types for handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
 import "./App.css";
 import { Button, Card, Form } from "react-bootstrap";
+import React, { useState } from "react";
 
-const App = () => (
-  <>
-    <Card>
-      <Card.Body>
-        <Form>
-          <Form.Group>
-            <Form.Label> Upload an image </Form.Label>
-            <Form.Control type="file" />
-          </Form.Group>
-          <Button variant="primary">Upload</Button>
-        </Form>
-      </Card.Body>
-    </Card>
-  </>
-);
+const App = () => {
+  const [imageFile, setImageFile] = useState<File>();
+  const [isPredicting, setIsPredicting] = useState(false);
+  const [imageSelected, setImageSelected] = useState(false);
+
+  const imageFieldChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setImageFile((event.target as HTMLInputElement).files![0]);
+    setImageSelected(true);
+  };
+
+  const detectFaces = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setIsPredicting(isPredicting);
+    console.log(imageFile);
+  };
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <Form>
+            <Form.Group>
+              <Form.Label>Upload an image</Form.Label>
+              <Form.Control
+                type="file"
+                accept="image/*"
+                onChange={imageFieldChangeHandler}
+              />
+            </Form.Group>
+            <Button
+              variant="primary"
+              onClick={detectFaces}
+              disabled={!imageSelected}
+            >
+              Upload
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
 export default App;
