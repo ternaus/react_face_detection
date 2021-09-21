78974ca5cf7f7fbc8cdd23465da4f099c70dc9be
Vladimir Iglovikov <Iglovikov@gmail.com>
2021-09-21 15:28:11 -0700

    Image Submit form is shown

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
+import { Button, Card, Form } from "react-bootstrap";
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Card>
+      <Card.Body>
+        <Form>
+          <Form.Group>
+            <Form.Label> Upload an image </Form.Label>
+            <Form.Control type="file" />
+          </Form.Group>
+          <Button variant="primary">Upload</Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  </>
+);
 
 export default App;
